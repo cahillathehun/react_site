b9ee2f54748b7a663fa10f0ff5440b771c9b6a6b
Alexander Cahill <dvt_cahal@CZL0174.local>
2023-03-16 15:51:23 +0100

    updated some text on homepage

--- src/Home.js
+++ src/Home.js
@@ -12,13 +12,18 @@ const Home = () => {
             <h2>Homepage</h2>
                 <br/>
                 <p>
-                    Welcome to my homepage on this fine {today}!
+                    Welcome to my page on this fine {today} morning/afternoon/evening!
                 </p>
                 <br/>
                 <p>
-                    I am using this webpage to learn react.
+                    I am using this webpage to learn react and arse about with hosting on Firebase and gitworkflows/ ops mgmt etc.
                 </p>
                 <br/>
+                <p>
+                    You can get in contact with me using via the LinkedIn link in the top corner there.
+                </p>
+                <br/>
+                
                 <p>
                     Plan is for the POEMS page to fetch a random poem and picture of the poet from some cloud storage and display on the page.
                 </p>
