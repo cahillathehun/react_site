f71c0ae7a987b80a6eb61dbfc8a540e4c8ab4530
Alexander Cahill <dvt_cahal@CZL0174.local>
2023-03-17 11:10:03 +0100

    some styling and add img

--- src/Poems.js
+++ src/Poems.js
@@ -2,39 +2,43 @@
 function Poems() {
     return (
     <div className="Poems">
-        <div className="poemContent">
-            <h2>POEMS</h2>
-            <br/>
-            <p>
-                !!!!!! Functionality is not finished yet, plan is for the button to fecth a random poem from a random author and show their picture on this page. !!!!!!
-            </p>
-            <br/>
-            <button>Click for a poem</button><br/>
-            <br/>
-            <p>Turning and turning in the widening gyre<br/>   
-                The falcon cannot hear the falconer;<br/>
-                Things fall apart; the centre cannot hold;<br/>
-                Mere anarchy is loosed upon the world,<br/>
-                The blood-dimmed tide is loosed, and everywhere<br/>
-                The ceremony of innocence is drowned;<br/>
-                The best lack all conviction, while the worst<br/>   
-                Are full of passionate intensity.<br/>
+        <h2>POEMS</h2>
+            <div className="poemContent">
                 <br/>
-                Surely some revelation is at hand;<br/>
-                Surely the Second Coming is at hand.<br/>
-                The Second Coming! Hardly are those words out<br/>   
-                When a vast image out of Spiritus Mundi<br/>
-                Troubles my sight: somewhere in sands of the desert<br/>   
-                A shape with lion body and the head of a man,<br/>   
-                A gaze blank and pitiless as the sun,<br/>   
-                Is moving its slow thighs, while all about it<br/>   
-                Reel shadows of the indignant desert birds.<br/>
-                The darkness drops again; but now I know<br/>
-                That twenty centuries of stony sleep<br/>
-                Were vexed to nightmare by a rocking cradle,<br/>
-                And what rough beast, its hour come round at last,<br/>
-                Slouches towards Bethlehem to be born?<br/>
-            </p>
+                <p>
+                    !!!!!! Functionality is not finished yet, plan is for the button to fecth a random poem from a random author and show their picture on this page. !!!!!!
+                </p>
+                <br/>
+                <button>Click for a poem</button><br/>
+                <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/8/88/William_Butler_Yeats_by_George_Charles_Beresford.jpg"/>
+                </div>
+                
+                <br/>
+                <p>Turning and turning in the widening gyre<br/>   
+                    The falcon cannot hear the falconer;<br/>
+                    Things fall apart; the centre cannot hold;<br/>
+                    Mere anarchy is loosed upon the world,<br/>
+                    The blood-dimmed tide is loosed, and everywhere<br/>
+                    The ceremony of innocence is drowned;<br/>
+                    The best lack all conviction, while the worst<br/>   
+                    Are full of passionate intensity.<br/>
+                    <br/>
+                    Surely some revelation is at hand;<br/>
+                    Surely the Second Coming is at hand.<br/>
+                    The Second Coming! Hardly are those words out<br/>   
+                    When a vast image out of Spiritus Mundi<br/>
+                    Troubles my sight: somewhere in sands of the desert<br/>   
+                    A shape with lion body and the head of a man,<br/>   
+                    A gaze blank and pitiless as the sun,<br/>   
+                    Is moving its slow thighs, while all about it<br/>   
+                    Reel shadows of the indignant desert birds.<br/>
+                    The darkness drops again; but now I know<br/>
+                    That twenty centuries of stony sleep<br/>
+                    Were vexed to nightmare by a rocking cradle,<br/>
+                    And what rough beast, its hour come round at last,<br/>
+                    Slouches towards Bethlehem to be born?<br/>
+                </p>
         </div>
     </div>
   );
