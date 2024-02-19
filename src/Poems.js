0f0696cb4d75a6b1f1d31b818ef7b80f3c5132fa
unknown <alex-cahill@hotmail.com>
2024-02-19 17:59:15 +0100

    update formatting.

--- src/Poems.js
+++ src/Poems.js
@@ -6,16 +6,17 @@ function Poems() {
     // TODO: move to different file/cloud storage
     const yeats = {
         SecondComing: "<p>Turning and turning in the widening gyre<br/>The falcon cannot hear the falconer;<br/>Things fall apart; the centre cannot hold;<br/>Mere anarchy is loosed upon the world,<br/>The blood-dimmed tide is loosed, and everywhere<br/>The ceremony of innocence is drowned;<br/>The best lack all conviction, while the worst<br/>Are full of passionate intensity.<br/><br/>Surely some revelation is at hand;<br/>Surely the Second Coming is at hand.<br/>The Second Coming! Hardly are those words out<br/>When a vast image out of Spiritus Mundi<br/>Troubles my sight: somewhere in sands of the desert<br/>A shape with lion body and the head of a man,<br/>A gaze blank and pitiless as the sun,<br/>Is moving its slow thighs, while all about it<br/>Reel shadows of the indignant desert birds.<br/>The darkness drops again; but now I know<br/>That twenty centuries of stony sleep<br/>Were vexed to nightmare by a rocking cradle,<br/>And what rough beast, its hour come round at last,<br/>Slouches towards Bethlehem to be born?<br/></p>",
-        ToAnIsleInTheWater: "<p>Shy one, shy one,<br/>Shy one of my heart,<br/>She moves in the firelight<br/>Pensively apart.<br/><br/>She carries in the dishes,<br/>And lays them in a row.<br/>To an isle in the water<br/>With her would I go.<br/><br/>She carries in the candles,<br/>And lights the curtained room,<br/>Shy in the doorway<br/>And shy in the gloom;<br/><br/>And shy as a rabbit,<br/>Helpful and shy.<br/>To an isle in the water<br/>With her would I fly.</p>"
+        ToAnIsleInTheWater: "<p>Shy one, shy one,<br/>Shy one of my heart,<br/>She moves in the firelight<br/>Pensively apart.<br/><br/>She carries in the dishes,<br/>And lays them in a row.<br/>To an isle in the water<br/>With her would I go.<br/><br/>She carries in the candles,<br/>And lights the curtained room,<br/>Shy in the doorway<br/>And shy in the gloom;<br/><br/>And shy as a rabbit,<br/>Helpful and shy.<br/>To an isle in the water<br/>With her would I fly.</p>",
+        LakeIsle: "<p>I will arise and go now, and go to Innisfree,<br>And a small cabin build there, of clay and wattles made;<br>Nine bean-rows will I have there, a hive for the honey-bee,<br>And live alone in the bee-loud glade.<br><br>And I shall have some peace there, for peace comes dropping slow,<br>Dropping from the veils of the morning to where the cricket sings;<br>There midnight’s all a glimmer, and noon a purple glow,<br>And evening full of the linnet’s wings.<br><br>I will arise and go now, for always night and day<br>I hear lake water lapping with low sounds by the shore;<br>While I stand on the roadway, or on the pavements grey,<br>I hear it in the deep heart’s core.<br><br><br></p>",
     };
     const joyce = {
         AFlowerGivenToMyDaughter: "<p>Frail the white rose and frail are,<br/>Her hands that gave<br/>Whose soul is sere and paler<br/>Than time's wan wave.<br/><br/>Rosefrail and fair -- yet frailest<br/>A wonder wild<br/>In gentle eyes thou veilest,<br/>My blueveined child.</p>"
     };
-    const rose_of_scotland = {
-      "<p>The rose of all the world is not for me.<br>I want for my part<br>Only the little white rose of Scotland<br>That smells sharp and sweet -- and breaks the heart.</p>"
+    const macdiarmid = {
+      LittleWhiteRose: "<p>The rose of all the world is not for me.<br>I want for my part<br>Only the little white rose of Scotland<br>That smells sharp and sweet -- and breaks the heart.</p>"
     };
     const lake_isle = {
-      "<p>I will arise and go now, and go to Innisfree,<br>And a small cabin build there, of clay and wattles made;<br>Nine bean-rows will I have there, a hive for the honey-bee,<br>And live alone in the bee-loud glade.<br><br>And I shall have some peace there, for peace comes dropping slow,<br>Dropping from the veils of the morning to where the cricket sings;<br>There midnight’s all a glimmer, and noon a purple glow,<br>And evening full of the linnet’s wings.<br><br>I will arise and go now, for always night and day<br>I hear lake water lapping with low sounds by the shore;<br>While I stand on the roadway, or on the pavements grey,<br>I hear it in the deep heart’s core.<br><br><br></p>"
+
     };
     const alist = [yeats, joyce, rose_of_scotland];
 
