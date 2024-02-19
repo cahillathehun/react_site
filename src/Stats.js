d443408e02613642c4381d716ad26dbb46c7b3d5
unknown <alex-cahill@hotmail.com>
2024-02-19 15:15:20 +0100

    remove style tag causing error.

--- src/Stats.js
+++ src/Stats.js
@@ -4,9 +4,9 @@ function Stats() {
         <div className="Stats">
           <h2>STATS</h2>
             <div className="Report">
-              <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/451f01d0-c759-4464-867e-3832df464aac/page/tEnnC" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+              <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/451f01d0-c759-4464-867e-3832df464aac/page/tEnnC" frameborder="0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
             </div>
-            
+
         </div>
   );
 }
