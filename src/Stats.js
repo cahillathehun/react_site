a1192a7c34ae2c7e66dcfa8cb9c667db29c89c30
unknown <alex-cahill@hotmail.com>
2024-02-19 15:01:08 +0100

    add header.

--- src/Stats.js
+++ src/Stats.js
@@ -1,9 +1,12 @@
 
 function Stats() {
     return (
-        <div className="App">
-          <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/451f01d0-c759-4464-867e-3832df464aac/page/tEnnC" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
-
+        <div className="Stats">
+          <h2>STATS</h2>
+            <div className="Report">
+              <iframe width="600" height="450" src="https://lookerstudio.google.com/embed/reporting/451f01d0-c759-4464-867e-3832df464aac/page/tEnnC" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+            </div>
+            
         </div>
   );
 }
